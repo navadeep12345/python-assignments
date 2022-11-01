676f3c4d2201920c9f86ba4e2408694b2abcc943
navadeep12345 <117149495+navadeep12345@users.noreply.github.com>
2022-11-01 12:26:25 +0530

    Add files via upload

---
+++ 9th answer.py
@@ -0,0 +1,6 @@
+#Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name="Navadeep Varma"
+age="20"
+qualification="Btech in computer science"
+years_of_experience="3 years"
+print(name,age,qualification,years_of_experience)
